f5130542782f26046165ebe2646b20a7619e82db
sparshkumar007 <sparshrajput92@gmail.com>
2023-04-29 16:23:39 +0530

    initial commit

---
+++ project.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class TIME
+{
+public:
+    int hr, min;
+    TIME()
+    {
+        hr = 0;
+        min = 0;
+    }
+    TIME(int _hr, int _min)
+    {
+        hr = _hr;
+        min = _min;
+    }
+};
+
+class drivers
+{
+public:
+    string name;
+    int age;
+    vector<int> schedule;
+    drivers() {}
+    drivers(string _name, int _age, vector<int> _schedule) {}
+};
+
+int main()
+{
+    vector<vector<int>> cities;
+    int dst = 2;
+    int requiredFlow = 35;
+    TIME req;
+    req.hr = 3;
+    req.min = 53;
+    bool pref = 1;
+    int choice;
+    // 1->pipes;
+    // 2->tankers;
+    choice = 1;
+    if (choice == 1)
+    {
+        network_flow(cities, dst, requiredFlow, req);
+    }
+    return 0;
+}
