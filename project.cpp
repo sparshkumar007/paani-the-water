a50f9db7f97a84be2d978a51fd1989e9660dfc85
sparshkumar007 <sparshrajput92@gmail.com>
2023-05-03 00:27:25 +0530

    network flow done

--- project.cpp
+++ project.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
 using namespace std;
 
 class TIME
@@ -28,11 +30,99 @@ public:
     drivers(string _name, int _age, vector<int> _schedule) {}
 };
 
+vector<vector<int>> cities;
+vector<int> srcs;
+int no_of_cities;
+
+void assign()
+{
+    cout << "assigned";
+}
+
+int Edmond_Karp(int dst)
+{
+    // assigning a dummy graph
+    // we'll make changes in dummy graph by changing reverse edges and forward edges
+    vector<vector<int>> dummy = cities;
+    int n = no_of_cities;
+
+    // algorithm
+    int ans = 0;
+    bool f = 1;
+    while (f)
+    {
+        f = 0;
+        // taken a temparory source having infinite flow to all the given sources.
+        // all our sources have parent=-2.
+        int flow = INT_MAX;
+        queue<pair<int, int>> q;
+        vector<int> parent(n, -1);
+        vector<bool> visited(n, 0);
+        for (auto it : srcs)
+        {
+            q.push(make_pair(it, -2));
+            visited[it] = 1;
+        }
+        while (!q.empty())
+        {
+            pair<int, int> x = q.front();
+            q.pop();
+            int y = x.first;
+            parent[y] = x.second;
+            for (int i = 0; i < n; i++)
+            {
+                if (dummy[y][i] && !visited[i])
+                {
+                    q.push(make_pair(y, i));
+                    visited[i] = 1;
+                }
+            }
+        }
+        if (parent[dst] != -1)
+        {
+            // as we have reached our destination so set f=1.
+            f = 1;
+            vector<int> path;
+            int x = 0;
+            int y = dst;
+            while (x != -2)
+            {
+                path.push_back(x);
+                x = parent[y];
+            }
+
+            // calculating flow from the path.
+            int flow = INT_MAX;
+            for (int i = 0; i < path.size() - 1; i++)
+            {
+                flow = min(flow, dummy[path[i + 1]][path[i]]);
+            }
+
+            // making changes in forward and revrse map.
+            for (int i = 0; i < path.size() - 1; i++)
+            {
+                // forward edge
+                dummy[path[i + 1]][path[i]] -= flow;
+                // reverse edge
+                dummy[path[i]][path[i + 1]] += flow;
+            }
+            ans += flow;
+        }
+    }
+    return ans;
+}
+
+int networkflow(int dst, int requiredflow)
+{
+    int flow_per_hour = Edmond_Karp(dst);
+    cout << flow_per_hour << endl;
+}
+
 int main()
 {
-    vector<vector<int>> cities;
+    assign();
     int dst = 2;
-    int requiredFlow = 35;
+    int requiredamount = 35;
     TIME req;
     req.hr = 3;
     req.min = 53;
@@ -41,10 +131,7 @@ int main()
     // 1->pipes;
     // 2->tankers;
     choice = 1;
-    if (choice == 1)
-    {
-        network_flow(cities, dst, requiredFlow, req);
-    }
+    int time_by_pipes = networkflow(dst, requiredamount);
+    int time_by_tanker = 0;
     return 0;
-    dfdffd
 }
